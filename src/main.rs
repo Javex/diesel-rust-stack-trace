4e7108e4b0653e357484619e8a0ac3033b2dab1c
javex <florian.ruechel@inexplicity.de>
2024-07-21 22:11:55 +0930

    smaller example

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,10 @@
 use diesel::prelude::*;
 
-#[derive(diesel::MultiConnection)]
-pub enum DbConnection {
-    Sqlite(diesel::SqliteConnection),
+pub fn get_result<U>(
+    conn: &mut diesel::SqliteConnection,
+    query: impl RunQueryDsl<SqliteConnection>,
+) -> QueryResult<U> {
+    query.get_result(conn)
 }
 
-impl DbConnection {
-    pub fn get_result<U>(&mut self, query: impl RunQueryDsl<DbConnection>) -> QueryResult<U> {
-        match self {
-            DbConnection::Sqlite(conn) => query.get_result(conn),
-        }
-    }
-}
-
-pub fn connect_sqlite(db: &str) -> DbConnection {
-    let conn = SqliteConnection::establish(&db).unwrap();
-    DbConnection::Sqlite(conn)
-}
-
-pub fn establish_connection() -> DbConnection {
-    let db = "test.db";
-    connect_sqlite(db)
-}
-fn main() {
-    establish_connection();
-}
+fn main() {}
